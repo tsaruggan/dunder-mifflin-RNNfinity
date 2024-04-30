ed0bac9cd847d49fbd429aa0265f54c1b1152459
tsaruggan <tsaruggan@gmail.com>
2024-04-30 14:36:08 -0400

    social image

--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -30,6 +30,7 @@ class App extends React.Component {
   };
 
   render() {
+    const socialThumbnailUrl = "https://img.buzzfeed.com/buzzfeed-static/static/2016-07/22/15/asset/buzzfeed-prod-fastlane01/sub-buzz-32165-1469216684-6.jpg";
     return (
       <>
         <Head>
@@ -37,7 +38,11 @@ class App extends React.Component {
           <meta name="description" content="A recurrent neural network that generates scripts for The Office" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
-
+          <meta property="og:title" content="Dunder Mifflin RNNfinity" />
+          <meta property="og:description" content="A recurrent neural network that generates scripts for The Office." />
+          <meta property="og:image" content={socialThumbnailUrl} />
+          <meta property="og:url" content="https://dunder-mifflin-rnnfinity.vercel.app/" /> {/* Replace with your site URL */}
+          <meta property="og:type" content="website" />
         </Head>
         <div className="App">
           <Header />
