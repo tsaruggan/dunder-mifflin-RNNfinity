bdd042fc69c043d27564493411349b3382e82e28
tsaruggan <tsaruggan@gmail.com>
2024-04-29 23:36:47 -0400

    bleh

--- demo/frontend/src/components/Header.js
+++ demo/frontend/src/components/Header.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className = "header">
-            <h1 id = "title">Dunder Mifflin RNNfinity</h1>
-            <p id = "description">A recurrent neural network that generates scripts for The Office</p>
+        <div className="header">
+            <h1 id="title">Dunder Mifflin RNNfinity</h1>
+            <p id="description">
+                A recurrent neural network that generates scripts for The Office &nbsp;
+                <a href="https://github.com/tsaruggan/dunder-mifflin-RNNfinity" target="_blank" rel="noopener noreferrer">
+                    [github]
+                </a>
+            </p>
+
         </div>
     )
 }
