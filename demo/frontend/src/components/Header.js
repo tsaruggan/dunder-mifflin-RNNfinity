5960a474f214bdbf55bbf6c7332b7104eb8d5c86
tsaruggan <tsaruggan@gmail.com>
2024-04-29 22:47:04 -0400

    added demo folder

---
+++ demo/frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className = "header">
+            <h1 id = "title">Dunder Mifflin RNNfinity</h1>
+            <p id = "description">A reccurrent neural network that generates scripts for The Office</p>
+        </div>
+    )
+}
+
+export default Header
